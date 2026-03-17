a13e4d66a22e19d5cd0482e680d2c8c34e66ae17
AlexanderMcMarshall <103285506+AlexanderMcMarshall@users.noreply.github.com>
2026-03-17 18:55:45 +0000

    Test

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react()],
-  base:"/junk-removal-mps/"
+  base:"/junk-removal-mps/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
